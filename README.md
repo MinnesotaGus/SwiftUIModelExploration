98475b554865266474a485adcd31da771aedb089
Jordan Gustafson <jordanrgustafson@gmail.com>
2020-06-30 17:18:03 -0500

    Get the Project Started

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SwiftUIModelExploration
+
 Project for exploring how to structure an apps model layer in SwiftUI
---
+++ SwiftUIModelExploration/SwiftUIModelExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIModelExplorationApp.swift
+//  SwiftUIModelExploration
+//
+//  Created by Jordan Gustafson on 6/30/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIModelExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
